81d2fc40d2990bbcefa9f926dbdebc633e23dc4c
idahamjith <induwaradahamjith2004@gmail.com>
2026-08-17 23:38:32 +0530

    Force update service worker cache bypass

--- js/push.js
+++ js/push.js
@@ -21,7 +21,7 @@ async function registerServiceWorker() {
     return null;
   }
   try {
-    const reg = await navigator.serviceWorker.register('./sw.js');
+    const reg = await navigator.serviceWorker.register('./sw.js?v=3');
     console.log('[FCM] Service Worker registered, scope:', reg.scope);
     return reg;
   } catch (err) {
